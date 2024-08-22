537403500b21b88168618d27b95865a8a97a4c86
SoumyaCode74 <soumyadeep.work01@gmail.com>
2024-08-23 00:38:22 +0530

    Basic fork function use

---
+++ processes/fork1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char * argv[]){
+	int val1 = 111;
+	if(argc > 1){
+		if(strstr(argv[1], "c")){
+			int id = fork();
+			switch(id){
+				case -1:
+					printf("No child process created!\n");
+					break;
+				case 0:
+					printf("Child process running with PID: %d\n", getpid());
+					printf("Parent process running with PID: %d\n", getppid());
+					val1 *= 2;
+					printf("val1=%d\n", val1);
+					break;
+				default:
+					printf("Parent process running with PID: %d\n", getpid());
+					val1 += 2;
+					printf("val1=%d\n", val1);
+					
+				}
+		}
+	}
+	else{
+		printf("Only 1 process running. No child process to create\n");
+	}
+	return 0;
+}
+
+
